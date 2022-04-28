acb043109a70706664833c23ca5cc482bf13d2cd
BenjaminGonvers <gonversbenjamin@gmail.com>
2022-04-28 16:32:32 +0200

    some test in PCG testy

---
+++ PCG/Assets/03_Scripts/01_Procedural/TreeNode.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace _03_Scripts._01_Procedural
+{
+    public class TreeNode : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    
+    
+    
+    }
+}
